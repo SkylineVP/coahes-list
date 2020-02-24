cb96cf27e83033acce1a835f4f6cbe08ee1c58d3
SkylineVP <vladpolizp@gmail.com>
2020-02-24 13:53:09 +0200

    Refactor SelectedCoach class to function

--- src/components/SelectedCoach/index.js
+++ src/components/SelectedCoach/index.js
@@ -1,18 +1,14 @@
 import React, {Component} from "react";
 import style              from "./styles.module.scss"
 
-class SelectedCoach extends Component {
-
-
-    render() {
-        return (
-            <div className={style.block}>
-                <div className={style.to}>To:</div>
-                <p className={style.family}>{this.props.coaches.map((coach) => `${coach.firstName} ${coach.lastName}`).join(', ')}</p>
-            </div>
-        );
-    }
-
+function SelectedCoach(props) {
+    const {coaches} = props;
+    return (
+        <div className={style.block}>
+            <div className={style.to}>To:</div>
+            <p className={style.family}>{coaches.map((coach) => `${coach.firstName} ${coach.lastName}`).join(', ')}</p>
+        </div>
+    );
 }
 
 export default SelectedCoach
